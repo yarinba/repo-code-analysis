0a2a5d752c075b74e05f1f70221e8ccdc30f4835
Yarin Barnes <yarinb7877@gmail.com>
2024-09-09 22:14:48 +0300

    add PATCH in cors methods

--- apps/be/src/main.ts
+++ apps/be/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: process.env.CLIENT_URL,
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       credentials: true,
     },
   });
